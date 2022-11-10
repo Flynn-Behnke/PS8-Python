6848622e1d156262b3764e87734211c08d8de186
Flynn-Behnke <113999225+Flynn-Behnke@users.noreply.github.com>
2022-11-09 20:36:56 -0600

    Update PS8P2.py

--- PS8P2.py
+++ PS8P2.py
@@ -2,10 +2,15 @@ def paintneeded(l, w, h,):
   sqfeet = (2*float(l)*float(w))+(2*float(w)*float(h))+(2*float(l)*float(h))
   galofpaint = sqfeet / 50
   return galofpaint
+  
+gate = str(input('Would you like to run this program? Yes or No?: '))
+while gate == 'Yes':
+  l = float(input("Enter length of room in feet: "))
+  w = float(input("Enter width of room in feet: "))
+  h = float(input("Enter height of room in feet: "))
 
-l = float(input("Enter length of room in feet: "))
-w = float(input("Enter width of room in feet: "))
-h = float(input("Enter height of room in feet: "))
+  paint = paintneeded(l, w, h)
+  print(paint, "gallons of paint needed")
+  gate = str(input("Would you like to run this program again? Yes or No?: "))
 
-paint = paintneeded(l, w, h)
-print(paint, "gallons of paint needed")
+print("Have a good day :)")
